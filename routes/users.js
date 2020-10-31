2ac5d48fd8baa9c79c781e0a65c74806eac4ce8a
Deepak joshi <deepak10995@gmail.com>
2020-11-01 02:29:38 +0530

    bcrypt setup on user database

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 import express from "express";
 const router = express.Router();
+import bcrypt from "bcryptjs";
+
+//User model
+import User from "../models/User";
 
 //Login Page
 
@@ -14,8 +18,67 @@ router.get("/register", (req, res) => {
 
 // Register Handle
 router.post("/register", (req, res) => {
-  console.log(req.body);
-  res.send("Hello register");
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
+
+  // Check required fields
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: "Please fill all the fields" });
+  }
+
+  if (password !== password2) {
+    errors.push({ msg: "password doesnt match" });
+  }
+
+  if (password.length < 6) {
+    errors.push({ msg: "Password should be atleast 6 characters" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", {
+      errors,
+      name,
+      email,
+      password,
+      password2,
+    });
+  } else {
+    // After passes all the validations
+    User.findOne({ email: email }).then((user) => {
+      if (user) {
+        // User exist
+        res.render("register", {
+          errors,
+          name,
+          email,
+          password,
+          password2,
+        });
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password,
+        });
+        // password bcrypt
+        bcrypt.genSalt(10, (err, salt) =>
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            // set password to hashed
+            newUser.password = hash;
+
+            // Save User
+            newUser
+              .save()
+              .then((user) => {
+                res.redirect("/login");
+              })
+              .catch((err) => console.log(err));
+          })
+        );
+      }
+    });
+  }
 });
 
 export default router;
